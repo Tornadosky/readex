fe4f83c23a5e62d2cac2777d067c1a9790c55b17
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-02-06 17:44:14 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-emerald-800'>Hi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;

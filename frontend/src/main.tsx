5b8562c695c998a4ba5e338cdbf382baa4f81b15
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-02-21 15:21:30 +0100

    Add PDF reader with highlights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import PdfViewer from './components/custom/book_page/PdfViewer.tsx'
 import MainPage from './components/custom/main_page/MainPage.tsx'
 import '../app/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MainPage userData={""} />
+    {/* <MainPage userData={""} /> */}
+    <PdfViewer />
   </React.StrictMode>,
 )
